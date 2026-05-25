7b3f6cbc23e3ac3321de0ee69c48281f7a16c827
LinxPhy <lin.aboagye@gmail.com>
2026-05-25 23:32:43 +0100

    sidebar nearly done

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -13,6 +13,10 @@ export default function Sidebar(
 ) {
 
     const [Genres, setGenres] = useState(genres.slice(0, 5));
+    const [Companies, setCompanies] = useState(companies.slice(0, 5));
+    const [Themes, setThemes] = useState(themes.slice(0, 5));
+
+
     const [OpenSections, setOpenSections] = useState({
         genre: true,
         platform: true,
@@ -30,10 +34,10 @@ export default function Sidebar(
 
     return (
         <aside className={styles.sidebar}>
-            {/* <div>
+            <div className={styles.filters} >
                 <h4>Filters</h4>
-                <button>Clear All</button>
-            </div> */}
+                <button className={styles.clear}>Clear</button>
+            </div>
             <div>
                 <div className={styles.dropdownHeader} onClick={() => toggleSection('genre')}>
                     <h4>Genre</h4>
@@ -60,48 +64,83 @@ export default function Sidebar(
 
             </div>
             <div>
-                <h4>Platform</h4>
-                <ul>
-                    {platforms && platforms.map((platform: Platform) => (
-                        <li key={platform.id}>
-                            <input type="checkbox" id={platform.id.toString()} name={platform.name} value={platform.id} />
-                            <label htmlFor={platform.id.toString()}>{platform.name}</label>
-                        </li>
-                    ))}
-                </ul>
+                <div className={styles.dropdownHeader} onClick={() => toggleSection('platform')}>
+                    <h4>Platform</h4>
+                    <span>{OpenSections.platform ? "▼" : "▶"}</span>
+                </div>
+
+                <div className={`${styles.dropdown} ${OpenSections.platform ? styles.open : styles.closed}`}>
+                    <ul>
+                        {platforms && platforms.map((platform: Platform) => (
+                            <li key={platform.id}>
+                                <input type="checkbox" id={platform.id.toString()} name={platform.name} value={platform.id} />
+                                <label htmlFor={platform.id.toString()}>{platform.name}</label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div>
-                <h4>Company</h4>
-                <ul>
-                    {companies && companies.map((company: Company) => (
-                        <li key={company.id}>
-                            <input type="checkbox" id={company.id.toString()} name={company.name} value={company.id} />
-                            <label htmlFor={company.id.toString()}>{company.name}</label>
-                        </li>
-                    ))}
-                </ul>
+                <div className={styles.dropdownHeader} onClick={() => toggleSection('company')}>
+                    <h4>Company</h4>
+                    <span>{OpenSections.company ? "▼" : "▶"}</span>
+                </div>
+
+                <div className={`${styles.dropdown} ${OpenSections.company ? styles.open : styles.closed}`}>
+                    <ul>
+                        {Companies && Companies.map((company: Company) => (
+                            <li key={company.id}>
+                                <input type="checkbox" id={company.id.toString()} name={company.name} value={company.id} />
+                                <label htmlFor={company.id.toString()}>{company.name}</label>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div style={{ display: OpenSections.company ? 'block' : 'none' }}>
+                        {Companies.length != companies.length ?
+                            <p onClick={() => setCompanies(companies)}>▼ Show More</p> :
+                            <p onClick={() => setCompanies(companies.slice(0, 5))}>▲ Show Less</p>
+                        }
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className={styles.dropdownHeader} onClick={() => toggleSection('theme')}>
+                    <h4>Theme</h4>
+                    <span>{OpenSections.theme ? "▼" : "▶"}</span>
+                </div>
+
+                <div className={`${styles.dropdown} ${OpenSections.theme ? styles.open : styles.closed}`}>
+                    <ul>
+                        {Themes && Themes.map((theme: Theme) => (
+                            <li key={theme.id}>
+                                <input type="checkbox" id={theme.id.toString()} name={theme.name} value={theme.id} />
+                                <label htmlFor={theme.id.toString()}>{theme.name}</label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div>
-                <h4>Theme</h4>
-                <ul>
-                    {themes && themes.map((theme: Theme) => (
-                        <li key={theme.id}>
-                            <input type="checkbox" id={theme.id.toString()} name={theme.name} value={theme.id} />
-                            <label htmlFor={theme.id.toString()}>{theme.name}</label>
-                        </li>
-                    ))}
-                </ul>
+                <div className={styles.dropdownHeader} onClick={() => toggleSection('mode')}>
+                    <h4>Mode</h4>
+                    <span>{OpenSections.mode ? "▼" : "▶"}</span>
+                </div>
+
+                <div className={`${styles.dropdown} ${OpenSections.mode ? styles.open : styles.closed}`}>
+                    <ul>                        
+                        {modes && modes.map((mode: Mode) => (
+                            <li key={mode.id}>
+                                <input type="checkbox" id={mode.id.toString()} name={mode.name} value={mode.id} />
+                                <label htmlFor={mode.id.toString()}>{mode.name}</label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div>
-                <h4>Mode</h4>
-                <ul>
-                    {modes && modes.map((mode: Mode) => (
-                        <li key={mode.id}>
-                            <input type="checkbox" id={mode.id.toString()} name={mode.name} value={mode.id} />
-                            <label htmlFor={mode.id.toString()}>{mode.name}</label>
-                        </li>
-                    ))}
-                </ul>
+                <button className={styles.apply}>Apply</button>
             </div>
         </aside>
     )
