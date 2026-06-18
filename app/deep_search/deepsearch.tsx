125a40af0faa403fdd7fc2c32ba651256f115ad5
LinxPhy <lin.aboagye@gmail.com>
2026-06-18 23:37:03 +0100

    working with grok ai

--- app/deep_search/deepsearch.tsx
+++ app/deep_search/deepsearch.tsx
@@ -1,22 +1,44 @@
 'use client'
 import { useEffect, useState } from 'react';
 import styles from './page.module.css'
+import { useQuery } from '@tanstack/react-query';
+
+const fetchGames = async ({ values, page } : { values: any, page: number }) => {
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/deepsearch`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ values, page }),
+    });
+
+    return response.json();
+
+}
 
 export default function DeepSearch({ data }: { data: any }) {
 
     const { genres, platforms, themes } = data
+    const [page, setPage]: any = useState(1);
+    const [similarGames, setSimilarGames]: any = useState("");
 
-    const [values, setValues] : any = useState({
+    const [values, setValues]: any = useState({
         search: '',
         platforms: [6],
         genres: [],
         themes: [],
-        similar_games: '',
+        similar_games: [],
     });
 
-    useEffect(() => {
-        console.log(values)
-    }, [values])
+    const { data: games, isLoading, isError } = useQuery({
+        queryKey: ['deepsearch', page],
+        queryFn: () => fetchGames({ values, page}),
+        enabled: page > 1,
+        refetchOnWindowFocus: false
+    })
+
+    console.log(games)
 
     return (
         <div className={styles.deepsearch}>
@@ -33,14 +55,15 @@ export default function DeepSearch({ data }: { data: any }) {
                         value={values.search}
                         onChange={(e) => setValues({ ...values, search: e.target.value })}
                         rows={10}
+                        maxLength={1000}
                     >
                     </textarea>
-                    <span>{values.search.length}/300</span>
+                    <span>{values.search.length}/1000</span>
                 </div>
 
                 <div className={styles.platforms}>
                     <h3>What platforms do you want to play on?</h3>
-                    <div> 
+                    <div>
                         {platforms.map((platform: any) => (
                             <div key={platform.id} className={styles.platform}>
                                 <p>{platform.alternative_name}</p>
@@ -51,16 +74,25 @@ export default function DeepSearch({ data }: { data: any }) {
 
                 <div className={styles.genres}>
                     <h3>Please enter your genres you enjoy (optional)</h3>
-                    {/* append to genres */}
-                    <select name="genres" id="genres" onChange={(e) => setValues({ ...values, genres: [...values.genres, e.target.value] })}>
+
+                    <select name="genres" id="genres"
+                        onChange={(e) => {
+                            const genre = genres.find((genre: any) => genre.id === parseInt(e.target.value));
+                            if (genre && !values.genres.includes(genre)) {
+                                setValues({ ...values, genres: [...values.genres, genre] });
+                            }
+                        }}
+
+                    >
+                        <option value="">Select a genre...</option>
                         {genres.map((genre: any) => (
                             <option key={genre.id} value={genre.id}>{genre.name}</option>
                         ))}
                     </select>
-                    
+
                     <div className={styles.content}>
                         {values?.genres?.map((genre: any) => (
-                            <div key={genre.id} className={styles.genre}>
+                            <div onClick={() => setValues({ ...values, genres: values.genres.filter((g: any) => g.id !== genre.id) })} key={genre.id} className={styles.genre}>
                                 <p>{genre.name}</p>
                             </div>
                         ))}
@@ -69,14 +101,22 @@ export default function DeepSearch({ data }: { data: any }) {
 
                 <div>
                     <h3>Please enter your themes you enjoy (optional)</h3>
-                    <select name="themes" id="themes">
+                    <select name="themes" id="themes"
+                        onChange={(e) => {
+                            const theme = themes.find((theme: any) => theme.id === parseInt(e.target.value));
+                            if (theme && !values.themes.includes(theme)) {
+                                setValues({ ...values, themes: [...values.themes, theme] });
+                            }
+                        }}
+                    >
+                        <option value="">Select a theme...</option>
                         {themes.map((theme: any) => (
                             <option key={theme.id} value={theme.id}>{theme.name}</option>
                         ))}
                     </select>
                     <div className={styles.content}>
-                        {values.themes.map((theme: any) => (
-                            <div key={theme.id} className={styles.theme}>
+                        {values?.themes?.map((theme: any) => (
+                            <div onClick={() => setValues({ ...values, themes: values.themes.filter((t: any) => t.id !== theme.id) })} key={theme.id} className={styles.theme}>
                                 <p>{theme.name}</p>
                             </div>
                         ))}
@@ -85,18 +125,44 @@ export default function DeepSearch({ data }: { data: any }) {
 
                 <div>
                     <h3>Please enter any similar games (optional)</h3>
-                    <input type="text" name="similar" placeholder="Red Dead Redemption 2, Elden Ring" id="similar" />
-                    <div className={styles.content}></div>
+                    <div className={styles.similarInput}>
+                        <input type="text" name="similar" value={similarGames} placeholder="Red Dead Redemption 2" id="similar"
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    // add if game is not in similar games
+                                    if (!values.similar_games.includes(e.currentTarget.value)) {
+                                        setValues({ ...values, similar_games: [...values.similar_games, e.currentTarget.value] });
+                                        e.currentTarget.value = '';
+                                    }
+                                }
+                            }}
+                            onChange={(e) => setSimilarGames(e.target.value)}
+                        />
+                        <button onClick={() => {
+                            // add if game is not in similar games
+                            if (!values.similar_games.includes(similarGames) && similarGames !== '') {
+                                setValues({ ...values, similar_games: [...values.similar_games, similarGames] });
+                                setSimilarGames('');
+                            }
+                        }}  type="button">
+                            +
+                        </button>
+                    </div>
+                    <div className={styles.content}>
+                        {values?.similar_games?.map((game: any, index: number) => (
+                            <div onClick={() => setValues({ ...values, similar_games: values.similar_games.filter((g: any) => g !== game) })} key={index} className={styles.similar}>
+                                <p>{game}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
 
-                <button className={styles.submit}>
+                <button className={styles.submit} onClick={() => setPage((page: number) => page + 1)}>
                     Start Deep Search
                 </button>
             </div>
-
-
-
         </div>
+
     )
 
 }
--- routes/deepsearch.js
+++ routes/deepsearch.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const OpenAI = require("openai")
 const { pool } = require('../scripts/db');
 const app = express();
+
 
 app.get('/api/getdeepsearch', async (req, res) => {
 
@@ -33,7 +35,61 @@ app.get('/api/getdeepsearch', async (req, res) => {
 
 })
 
+const client = new OpenAI({
+    apiKey: process.env.GROK_API_KEY,
+    baseURL: "https://api.x.ai/v1",
+})
+
 app.post('/api/deepsearch', async (req, res) => {
+
+    try {
+        const { values, page, prevGames } = req.body
+        const { search, platforms, genres, themes, similar_games } = values
+
+        // const response = await client.responses.create({
+        //     model: 'grok-4.3',
+        //     input: [
+        //         { role: 'system', content: 'You are Grok, a highly intelligent, helpful AI assistant.' },
+        //         { role: 'user', content: 'What is the meaning of life, the universe, and everything?' },
+        //     ]
+        // })
+
+        const prompt = 
+        `
+            You are a Video Game Identifier, your role is to find a game recommendation that matches the following criteria:
+            Search: ${search}
+            Platforms: ${platforms}
+            Genres: ${genres}
+            Themes: ${themes}
+            Similar Games: ${similar_games}
+            You need to return a list of 10 games that match the criteria and the following
+            - Name
+            - Summary
+            - Genres (up to 5)
+            - Themes (up to 5)
+            - Platforms (up to 5 - most popular)
+            - Release Date (if applicable)
+            Please do not return any other information.
+            Do not include any of the following games in your response: ${prevGames}
+        `
+
+        console.log(prompt)
+
+        const response = await client.completions.create({
+            model: 'grok-4.3',
+            input: [
+                { role: 'system', content: 'You are Grok, a highly intelligent, helpful AI assistant.' },
+                { role: 'user', content: prompt },
+            ]
+        })
+
+        res.send({ data: response.choices[0].text, page , prevGames })
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
 })
 
 module.exports = app
